380b2792e63a98c515eb108dde49c5cec32779dd
Lorn <laurynnryerse@gmail.com>
2019-12-02 16:38:32 -0700

    done, finally.

--- answer.js
+++ answer.js
@@ -1,1 +1,19 @@
 // *** YOUR ANSWER BELOW ***
+"use strict";
+
+function negate(x){
+    return "un" + x;
+}
+console.log(negate("cold"));
+
+function intensify(x){
+    return "plus" + x;
+}
+console.log(intensify("cold"));
+
+function reinforce(x){
+    return "double" + x;
+}
+console.log(reinforce(intensify("cold")));
+
+console.log(reinforce(intensify(negate("cold"))));
